d0f8e62ca873828b00a8682424d62e7e978fadb5
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-12-03 12:28:32 +0330

    start project Project Routing implementation

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero,
+        officiis? Ducimus veniam tempora quaerat incidunt, accusantium sunt
+        voluptatem fugit, facere, quos obcaecati sint alias corrupti at pariatur
+        sit commodi debitis?
+      </h1>
+    </div>
+  );
+}
