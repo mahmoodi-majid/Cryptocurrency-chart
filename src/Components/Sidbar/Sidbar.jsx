a66dfd41c4991114c8f1bf1a547987f21fbd7e2a
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-12-04 22:40:33 +0330

    Fix Sidebar

--- src/Components/Sidbar/Sidbar.jsx
+++ src/Components/Sidbar/Sidbar.jsx
@@ -11,6 +11,9 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
 import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
 import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+import ManageAccountsOutlinedIcon from "@mui/icons-material/ManageAccountsOutlined";
+import ReportGmailerrorredOutlinedIcon from "@mui/icons-material/ReportGmailerrorredOutlined";
+import { Link } from "react-router-dom";
 
 export default function Sidbar() {
   return (
@@ -19,10 +22,12 @@ export default function Sidbar() {
         <div className="sidbarMenu">
           <h3 className="sidbarTitele">Dashborad</h3>
           <ul className="sidbarList">
-            <li className="sidbarListItem">
-              <LinStyleIcon className="sidbarIcon" active />
-              Home
-            </li>
+            <Link to="/" className="link">
+              <li className="sidbarListItem">
+                <LinStyleIcon className="sidbarIcon" active />
+                Home
+              </li>
+            </Link>
             <li className="sidbarListItem">
               <TimelineIcon className="sidbarIcon" />
               Analytics
@@ -36,18 +41,24 @@ export default function Sidbar() {
         <div className="sidbarMenu">
           <h3 className="sidbarTitele">Quick Menu</h3>
           <ul className="sidbarList">
-            <li className="sidbarListItem">
-              <PermIdentityIcon className="sidbarIcon" active />
-              User
-            </li>
-            <li className="sidbarListItem">
-              <StorefrontIcon className="sidbarIcon" />
-              New User
-            </li>
-            <li className="sidbarListItem">
-              <AttachMoneyIcon className="sidbarIcon" />
-              Products
-            </li>
+            <Link to="/users" className="link">
+              <li className="sidbarListItem">
+                <PermIdentityIcon className="sidbarIcon" active />
+                User
+              </li>
+            </Link>
+            <Link to="/newUser" className="link">
+              <li className="sidbarListItem">
+                <StorefrontIcon className="sidbarIcon" />
+                New User
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidbarListItem">
+                <AttachMoneyIcon className="sidbarIcon" />
+                Products
+              </li>
+            </Link>
             <li className="sidbarListItem">
               <BarChartIcon className="sidbarIcon" />
               Transaction
@@ -73,6 +84,24 @@ export default function Sidbar() {
             <li className="sidbarListItem">
               <MessageOutlinedIcon className="sidbarIcon" />
               Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidbarMenu">
+          <h3 className="sidbarTitele">Staff</h3>
+          <ul className="sidbarList">
+            <li className="sidbarListItem">
+              <ManageAccountsOutlinedIcon className="sidbarIcon" active />
+              Manage
+            </li>
+
+            <li className="sidbarListItem">
+              <TimelineIcon className="sidbarIcon" />
+              Analytics
+            </li>
+            <li className="sidbarListItem">
+              <ReportGmailerrorredOutlinedIcon className="sidbarIcon" />
+              Reports
             </li>
           </ul>
         </div>
