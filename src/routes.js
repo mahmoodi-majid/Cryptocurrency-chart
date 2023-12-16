f63277de264b4dd24ec1b0bba861f8ab4029a41b
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-12-16 16:11:11 +0330

    Product component coding to display product details

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Home from "./pages/Home/Home";
 import UserList from "./pages/Users/UserList";
 import NewUser from "./pages/NewUser/NewUser";
 import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
 
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/users", element: <UserList /> },
   { path: "/newUser", element: <NewUser /> },
   { path: "/products", element: <Products /> },
+  { path: "/product/:producId", element: <Product /> },
 ];
 
 export default routes;
