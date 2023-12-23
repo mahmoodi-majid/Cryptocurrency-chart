de7d1a2bb9393cd18d80b1d2a16099c98bd5d58e
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-12-23 11:49:49 +0330

    Coding the Product component to display product details Coding the Product component to display product details

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,6 +3,7 @@ import "./product.css";
 import { Link } from "react-router-dom";
 import Chart from "./../../Components/Chart/Chart";
 import { productsData } from "../../data";
+import Publish from "@mui/icons-material/Publish";
 
 export default function Product() {
   return (
@@ -24,28 +25,57 @@ export default function Product() {
             <span className="productName">Dell Labtop</span>
           </div>
           <div className="productInfoBottom">
-              <div className="productInfoItem">
-                <div className="productInfoKey">ID: </div>
-                <div className="productInfoValue">132</div>
-              </div>
-              <div className="productInfoItem">
-                <div className="productInfoKey">Name: </div>
-                <div className="productInfoValue">Dell Laptop</div>
-              </div>
-              <div className="productInfoItem">
-                <div className="productInfoKey">Sales: </div>
-                <div className="productInfoValue">$90000</div>
-              </div>
-              <div className="productInfoItem">
-                <div className="productInfoKey">Active: </div>
-                <div className="productInfoValue">Yes</div>
-              </div>
-              <div className="productInfoItem">
-                <div className="productInfoKey">In Stock: </div>
-                <div className="productInfoValue">No</div>
-              </div>
+            <div className="productInfoItem">
+              <div className="productInfoKey">ID: </div>
+              <div className="productInfoValue">132</div>
             </div>
+            <div className="productInfoItem">
+              <div className="productInfoKey">Name: </div>
+              <div className="productInfoValue">Dell Laptop</div>
+            </div>
+            <div className="productInfoItem">
+              <div className="productInfoKey">Sales: </div>
+              <div className="productInfoValue">$90000</div>
+            </div>
+            <div className="productInfoItem">
+              <div className="productInfoKey">Active: </div>
+              <div className="productInfoValue">Yes</div>
+            </div>
+            <div className="productInfoItem">
+              <div className="productInfoKey">In Stock: </div>
+              <div className="productInfoValue">No</div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="productBottom">
+        <form className="productFrom">
+          <div className="productFormLeft">
+            <label>Product Name</label>
+            <input type="text" placeholder="Dell labtop" />
+            <label>In Stock</label>
+            <select id="inStock">
+              <option value="yes">Yse</option>
+              <option value="no">No</option>
+            </select>
+
+            <label>Active</label>
+            <select id="inStock">
+              <option value="yes">Yse</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpLoader">
+              <img src="/Imgs/acer.jpg" alt="" className="productUpLoaderImg" />
+              <label>
+                <Publish />
+              </label>
+              <input type="file" style={{display:"none"}}/>
+            </div>
+          <button className="productButton">Uplod</button>
+          </div>
+        </form>
       </div>
     </div>
   );
